e9dab187246c6492077eb8a2120dc4a4392e1a9c
Nischal Shrestha <developer.nish@gmail.com>
2018-11-11 19:25:43 +1100

    fix

--- src/App.js
+++ src/App.js
@@ -1,33 +1,18 @@
-<<<<<<< HEAD
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="container">
         <div className="row">
-          <div className="col-sm">
-            One of three columns
-          </div>
-          <div className="col-sm">
-            One of three columns
-          </div>
-          <div className="col-sm">
-            One of three columns
-          </div>
+          <div className="col-sm">One of three columns</div>
+          <div className="col-sm">One of three columns</div>
+          <div className="col-sm">One of three columns</div>
         </div>
       </div>
     );
   }
-=======
-import React, { Component } from "react";
-
-class App extends Component {
-    render() {
-        return <div className="App" />;
-    }
->>>>>>> 4596e23e81fa35484b8279ae0858a49da63db9d6
 }
 
 export default App;
