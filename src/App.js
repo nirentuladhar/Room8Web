500348d482456661608bee213170e51e36cf820c
nirentuladhar <nirentuladhar@gmail.com>
2018-11-11 19:22:47 +1100

    Redux setup

--- src/App.js
+++ src/App.js
@@ -1,33 +1,17 @@
-<<<<<<< HEAD
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import Button from "./components/Button/button";
 
 class App extends Component {
   render() {
     return (
       <div className="container">
         <div className="row">
-          <div className="col-sm">
-            One of three columns
-          </div>
-          <div className="col-sm">
-            One of three columns
-          </div>
-          <div className="col-sm">
-            One of three columns
-          </div>
+          <Button></Button>
         </div>
+
       </div>
     );
   }
-=======
-import React, { Component } from "react";
-
-class App extends Component {
-    render() {
-        return <div className="App" />;
-    }
->>>>>>> 4596e23e81fa35484b8279ae0858a49da63db9d6
 }
 
 export default App;
---
+++ src/actions/loginAction.jsx
@@ -0,0 +1,5 @@
+export const buttonClick = () => {
+  return {
+    type: "BUTTON_CLICK"
+  }
+}
